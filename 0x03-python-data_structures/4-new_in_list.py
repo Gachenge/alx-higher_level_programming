27c43b44628f3d73b4a1da507a20f5ac73445fb9
Gachenge <gachenge1@gmail.com>
2022-12-04 13:32:02 +0000

    replace at and keep original

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if 0 > idx < (len(my_list) - 1):
+        return (my_list)
     new_list = my_list[:idx] + [element] + my_list[idx + 1:]
     return (new_list)
