543d1589bd9f726435b39110ad5643c1513a5002
Gachenge <gachenge1@gmail.com>
2022-12-07 12:27:55 +0000

    roman numerals

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    int = 0
+
+    for i in range(len(roman_string)):
+        if i != len(roman_string) - 1 and rom[roman_string[i]] < rom[roman_string[i + 1]]:
+            int += rom[roman_string[i]] * -1
+        else:
+            int += rom[roman_string[i]]
+    return int
