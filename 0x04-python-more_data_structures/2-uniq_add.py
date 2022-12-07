c041ecd3f63550195d00f7d314fe58a96f026aba
Gachenge <gachenge1@gmail.com>
2022-12-07 09:19:10 +0000

    sets and dictionaries

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unq = []
+    sum = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in unq:
+            unq.append(my_list[i])
+    for i in range(len(unq)):
+        sum += unq[i]
+    return sum
