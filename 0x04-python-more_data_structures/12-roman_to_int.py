461925a8546d8e801a3c17547f7a3969e99a5002
Gachenge <gachenge1@gmail.com>
2022-12-08 19:07:43 +0000

    check if none

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return None
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
     int = 0
-
-    if roman_string is None:
-        return None
     for i in range(len(roman_string)):
         if (i != len(roman_string) - 1 and rom[roman_string[i]] <
                 rom[roman_string[i + 1]]):
