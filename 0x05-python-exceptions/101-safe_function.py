b4be4e49fab592e28cb3b422fe5ec38b0d25cb5e
Gachenge <gachenge1@gmail.com>
2022-12-19 16:57:48 +0000

    execute a function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ import sys
 def safe_function(fct, *args):
     try:
         return(fct(*args))
-    except  Exception as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
