dafe3ff78717d32dc003a5e5ce7763492a169cbc
Gachenge <gachenge1@gmail.com>
2022-12-19 10:28:01 +0000

    print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    val = False
+    try:
+        print("{:d}".format(value))
+        val = True
+    except:
+        print("{}".format(value))
+    return (val)
