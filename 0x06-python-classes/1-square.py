7526289d0fc030267354d7161ebc7d94c551fabd
Gachenge <gachenge1@gmail.com>
2022-12-20 10:04:12 +0000

    pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""define a class square"""
+
+
 class Square:
     """define a private instance attribute"""
+
     def __init__(self, size):
         """ initialise the square"""
         self._size = size
