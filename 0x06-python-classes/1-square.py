02e4e0ee5b6f3a1a2d800286ea6cff2d59020f70
Gachenge <gachenge1@gmail.com>
2022-12-20 09:51:59 +0000

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """define a private instance attribute"""
+    def __init__(self, size):
+        """ initialise the square"""
+        self._size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Starting object oriented programming
