0dd20ecfbdc4b6646ea73b682cf8d5bb23128741
Gachenge <gachenge1@gmail.com>
2022-12-20 11:06:55 +0000

    pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,9 @@ class Square:
         """ initialise the square
         size: how big
         """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
-        self.__size = size
+        else:
+            self.__size = size
