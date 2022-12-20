de2fe6d55b3684adf84bdb24c8ec7dc33fd5092b
Gachenge <gachenge1@gmail.com>
2022-12-20 10:57:40 +0000

    pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """a new class square"""
 
+
 class Square:
     """define a square"""
+
     def __init__(self, size=0):
         """ initialise the square
         size: how big
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
