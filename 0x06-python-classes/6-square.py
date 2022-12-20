96db71dbb359e0e0acf13724e958dba52ca4084c
Gachenge <gachenge1@gmail.com>
2022-12-20 17:10:10 +0000

    correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
         """ initialise the square
         size: how big
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -22,7 +22,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__position = value
+        self.__size = value
 
     @property
     def position(self):
