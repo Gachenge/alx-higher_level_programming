b91ee822ecb782e488b9775549fbe1f797b8c63d
Gachenge <gachenge1@gmail.com>
2022-12-21 06:25:36 +0000

    correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,11 +42,12 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(' ', end='')
-            for k in range(self.__size):
-                print('#', end='')
-            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(' ', end='')
+                for k in range(self.__size):
+                    print('#', end='')
+                print()
