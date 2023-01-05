efb3c43f07559cc0f2fb7855605aefdf051aae80
Gachenge <gachenge1@gmail.com>
2023-01-05 14:40:25 +0000

    more tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,8 +19,8 @@ def matrix_divided(matrix, div):
             not all(isinstance(ele, list) for ele in matrix) or
             not all(isinstance(x, (int, float))
                     for x in [n for ele in matrix for n in ele])):
-        raise TypeError("matrix must be a matrix
-                        (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix"
+                        " (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
