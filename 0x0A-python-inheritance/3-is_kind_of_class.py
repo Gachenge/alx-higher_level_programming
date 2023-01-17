3436725430a2ede3fe69175ed5fd247c73516187
Gachenge <gachenge1@gmail.com>
2023-01-17 13:36:30 +0000

    if instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""check if its instance or inherited from"""
+
+def is_kind_of_class(obj, a_class):
+    """returns true or false if inherited or instance"""
+
+    return(isinstance(obj, a_class))
