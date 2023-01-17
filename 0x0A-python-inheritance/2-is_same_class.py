d9d0e5ec553d4f4c3e214927df343e0cdd8546f3
Gachenge <gachenge1@gmail.com>
2023-01-17 13:26:51 +0000

    check if instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function to check if its instance of """
+
+
+def is_same_class(obj, a_class):
+    """check if object is an instance of class"""
+    return(isinstance(obj, a_class))
