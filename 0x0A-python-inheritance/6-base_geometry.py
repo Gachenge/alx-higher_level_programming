5d7df57c8a40f252f1ba59ac409c1a59aa5d1e9e
Gachenge <gachenge1@gmail.com>
2023-01-17 14:26:23 +0000

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""based on the previously created empty class"""
+
+
+class BaseGeometry:
+    """implement the class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
