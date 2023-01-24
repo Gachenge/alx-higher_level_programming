1ab1cd9520105a9a97d3767532d0b52444b86faf
Gachenge <gachenge1@gmail.com>
2023-01-24 08:10:50 +0000

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read a text file"""
+
 
 def read_file(filename=""):
-    """read the contents of a file"""
+    """read the contents of a file and print them out"""
     with open(filename, encoding='utf8') as f:
-        print(f.read(), end ='')
+        print(f.read(), end='')
