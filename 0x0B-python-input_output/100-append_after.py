430e5e939453f063f7754e042fc8129ead4bce63
Gachenge <gachenge1@gmail.com>
2023-01-24 17:40:58 +0000

    pycodestyle

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """function to insert text in text after"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """search text and after a string, insert new strin
     won't manage file permissions or non existent file
     """
     content = ''
     with open(filename) as f:
-         for line in f:
-             content += line
-             if search_string in line:
-                 content += new_string
+        for line in f:
+            content += line
+            if search_string in line:
+                content += new_string
     with open(filename, 'w') as f:
         f.write(content)
