f42d0def01a6ff5515a7064e1ec61dc4729c51f6
Gachenge <gachenge1@gmail.com>
2023-01-24 16:30:28 +0000

    pycodestyle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function that returns a list of integers"""
 
+
 def pascal_triangle(n):
     """returns a list of integers representing the pascal teiangle
     returns empty list if ln <= 0
@@ -8,8 +9,8 @@ def pascal_triangle(n):
     pascal = []
 
     for i in range(n):
-        pascal.append([1]*(i + 1))
+        pascal.append([1] * (i + 1))
     for i in range(2, n):
         for j in range(1, i):
-            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i -1][j]
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
     return pascal
