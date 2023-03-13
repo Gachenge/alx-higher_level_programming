5cdedbd798643817b2f9585b1bbeaf448e09db89
Gachenge <gachenge1@gmail.com>
2023-03-13 16:33:36 +0300

    correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,27 +3,25 @@
 // find the second biggest in a list
 
 function secbig (arr) {
-	let max = arr[0];
-	let sec = arr[0];
+  let max = arr[0];
+  let sec = arr[0];
 
-	for (let i = 0; i < arr.length - 1; i++){
-		if (arr[i] > max){
-			sec = max;
-			max = arr[i];
-		}
-		else if (arr[i] > sec && arr[i] != max){
-			sec = arr[i];
-		}
-	}
-	return sec;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > max) {
+      sec = max;
+      max = arr[i];
+    } else if (arr[i] > sec && arr[i] !== max) {
+      sec = arr[i];
+    }
+  }
+  return sec;
 }
-let arr = []
-for (let i = 2; i < process.argv.length; i++){
-	arr.push(process.argv[i]);
+const arr = [];
+for (let i = 2; i < process.argv.length; i++) {
+  arr.push(process.argv[i]);
 }
-if (process.argv.length <= 3){
-	console.log(0);
-}
-else{
-	console.log(secbig(arr));
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  console.log(secbig(arr));
 }
