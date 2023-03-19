d1876a484a2b5765fdba90504ddac98035da50b4
Gachenge <gachenge1@gmail.com>
2023-03-19 13:13:58 +0300

    corrections

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    filter and print out all states starting with 'N'
-    order them by state.id
+script takes arguments
+filter and print out all states starting with 'N'
+order them by state.id
 """
 import sys
 import MySQLdb
@@ -9,7 +10,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+         ORDER BY states.id ASC".format(sys.argv[4]))
     for state in cursor.fetchall():
-        if (state[1] == sys.argv[4]):
-            print(state)
+        print(state)
