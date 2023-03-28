784054f521967041fa45ef069cf13b56f5d27d8d
Gachenge <gachenge1@gmail.com>
2023-03-28 17:19:33 +0300

    clean up

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@
 const fs = require('fs');
 const fileA = fs.readFileSync(process.argv[2], 'utf8');
 const fileB = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], fileA + '\n' + fileB);
+fs.writeFileSync(process.argv[4], fileA + fileB);
