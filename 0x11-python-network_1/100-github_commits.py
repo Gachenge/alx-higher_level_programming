9a2f79b5eb5ae56f844b91e8ac1b4c0b0b6c0260
Gachenge <gachenge1@gmail.com>
2023-04-01 11:06:52 +0300

    corrections

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     request = requests.get("https://api.github.com/repos/{}/{}/commits".
-                           format(argv[1], argv[2]))
+                           format(argv[2], argv[1]))
     if (request.status_code == 200):
         request_json = request.json()[:10]
         for commit in request_json:
